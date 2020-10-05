e3b3c122a2b8f273c0b29dfc5213764135cbda5a
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2020-10-05 20:30:53 +0800

    Mobile Projects

--- src/components/CyCraftHomePage.vue
+++ src/components/CyCraftHomePage.vue
@@ -13,30 +13,37 @@
       <button class="text-xl text-white" @click="clicked">Contact</button>
     </div>
     <!-- Hero Title -->
-    <div class="flex-col justify-center content-center m-10">
+    <div class="flex-col justify-center content-center m-10 pb-10">
       <div class="text-h1 text-white">We Craft without cruft.</div>
-      <div class="text-h4 text-white">We craft beautiful apps and strive for a better web.</div>
+      <div class="text-xl font-regular tracking-wide text-white pt-6">We craft beautiful apps and strive for a better web.</div>
       <button class="bg-white hover:bg-gray-400 text-black font-bold text-base uppercase py-3 px-4 inline-flex items-center my-20" @click="clicked">
         <svg class="fill-current w-5 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.9 17"><path d="M11.332,2.734V0h-8.5V2.734H0v8.2H2.832V13.9H5.9V10.937H8.5V13.9h3.07V10.937H17v-8.2Zm-2.595,5.7H5.9V5.7H8.737Zm5.43,0H11.332V5.7h2.835Z"/></svg>
         <span>Contact us</span>
       </button>
     </div>
-
-<!-- <svg xmlns="http://www.w3.org/2000/svg" width="13.9" height="17" viewBox="0 0 13.9 17">
-  <g id="Monster" transform="translate(13.9) rotate(90)">
-    <g id="Layer_1" data-name="Layer 1">
-      <path id="Path_18" data-name="Path 18" d="M11.332,2.734V0h-8.5V2.734H0v8.2H2.832V13.9H5.9V10.937H8.5V13.9h3.07V10.937H17v-8.2Zm-2.595,5.7H5.9V5.7H8.737Zm5.43,0H11.332V5.7h2.835Z"/>
-    </g>
-  </g>
-</svg> -->
+    <!-- Projects Section -->
+    <div class="mx-10">
+      <!-- Section Title -->
+      <div class="flex justify-start mb-6">
+        <div class="text-xl font-regular text-white tracking-wide">Projects</div>
+      </div>
+      <!-- Project Grid -->
+      <div class="grid grid-cols-1">
+        <project v-for="p in 4" :key="p" title="popicons" img="../assets/martin.jpg" class="py-3 mb-8"/>
+      </div>
+    </div>
 
   </div>
 </div>
 </template>
 
 <script>
+import Project from "./Project.vue";
 export default {
   name: 'CycraftHomePage',
+  components:{
+    Project,
+  },
   props: {},
   methods: {
     clicked(){
---
+++ src/components/Project.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="flex-col">     
+        <div class="title-container">
+            <span class="text-white uppercase text-xs tracking-widest">{{title}}</span>
+            <div class="line"></div>
+        </div>
+        <div class="relative mx-auto max-w-sm max-h-sm mt-3" style="padding-bottom: 100%">
+            <img class="absolute h-full w-full object-cover" src="../assets/martin.jpg" alt="Project Photo">
+        </div>
+    </div>
+
+</template>
+<style lang="sass" scoped>
+//$
+// .text-md
+//   +t-h5()
+//   color: #fff
+// .text-sm
+//   +t-h5()
+//   color: #fff
+//   font-size: 11px
+//   line-height: 15px
+.title-container
+  display: flex
+  align-items: center
+.line
+  background: white
+  height: 1px
+  margin-left: 6px
+  width: 100%
+</style>
+<script>
+export default {
+  name: 'Project',
+  props: {
+    textsm: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+        type: String,
+        default: ''
+    },
+    img: {
+        type: String,
+      default: ''
+    }
+  },
+}
+</script>
