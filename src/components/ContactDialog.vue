009d096d17fc6374c8a3ae402b591687ce524a48
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2020-12-17 15:01:55 +0800

    Contact Dialog complete

--- src/components/ContactDialog.vue
+++ src/components/ContactDialog.vue
@@ -39,7 +39,23 @@
           <div>
             <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full">
 
+              <svg
+                v-if="copied"
+                class="h-10 w-10 text-green-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
               <img
+                v-else
                 class="h-10 w-10"
                 src="/cy-icon-white-on-black.svg"
                 alt=""
@@ -58,6 +74,23 @@
                 <a href="mailto:luca@cycraft.co">
                   <CyButtonSimple @click="close">Email (Default)</CyButtonSimple>
                 </a>
+              </div>
+              <div class="mt-2 grid grid-cols-3 gap-2">
+
+                <CyButtonSimple
+                  class="mr-2 col-span-1"
+                  @click="copy"
+                >Copy</CyButtonSimple>
+                <span class="col-span-2 bg-gray-100 flex flex-row justify-center">
+                  <input
+                    id="copy-email"
+                    class="h-full w-full text-center bg-gray-100 px-1 text-medium focus:outline-none text-gray-500"
+                    type="text"
+                    value="luca@cycraft.co"
+                    readonly
+                  >
+                </span>
+
               </div>
             </div>
           </div>
@@ -91,9 +124,22 @@ export default {
       default: false,
     },
   },
+  data() {
+    return {
+      copied: false,
+    }
+  },
   methods: {
     close() {
       this.$emit('close')
+      setTimeout(() => (this.copied = false), 1000)
+    },
+    copy() {
+      let copyText = document.getElementById('copy-email')
+      copyText.select()
+      copyText.setSelectionRange(0, 99999)
+      document.execCommand('copy')
+      this.copied = true
     },
   },
 }
--- src/components/CyButtonSimple.vue
+++ src/components/CyButtonSimple.vue
@@ -1,23 +1,22 @@
 <template>
   <button
     id="cy-button"
-    class="relative btn bg-white border-2 border-white focus:outline-none hover:bg-btn-light-green focus:border-btn-bright-green text-h5 text-black"
+    class="simple-btn bg-white border-2 border-white focus:outline-none hover:bg-btn-light-green focus:border-btn-bright-green text-h5 text-black"
     v-bind="$attrs"
     v-on="$listeners"
   >
-    <div class="absolute inset-0">
-      <div class="inline-flex items-center py-3 px-4">
-        <svg
-          class="fill-current w-5 h-4 mr-2"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 13.9 17"
-        >
-          <path d="M11.332,2.734V0h-8.5V2.734H0v8.2H2.832V13.9H5.9V10.937H8.5V13.9h3.07V10.937H17v-8.2Zm-2.595,5.7H5.9V5.7H8.737Zm5.43,0H11.332V5.7h2.835Z" />
-        </svg>
-        <span>
-          <slot></slot>
-        </span>
-      </div>
+
+    <div class="inline-flex items-center py-3 px-1">
+      <svg
+        class="fill-current w-5 h-4 mr-2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 13.9 17"
+      >
+        <path d="M11.332,2.734V0h-8.5V2.734H0v8.2H2.832V13.9H5.9V10.937H8.5V13.9h3.07V10.937H17v-8.2Zm-2.595,5.7H5.9V5.7H8.737Zm5.43,0H11.332V5.7h2.835Z" />
+      </svg>
+      <span>
+        <slot></slot>
+      </span>
     </div>
 
   </button>
@@ -29,7 +28,7 @@ export default {
 }
 </script>
 <style lang="sass">
-.btn
+.simple-btn
   width: 100%
   height: 46px
 </style>
