f44beeca013b2abbf19027aa6ead8b5073e8b048
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2020-12-23 14:45:46 +0800

    lottie animation working, but unsure if correct and not fullscreen

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,55 @@
 <template>
-  <div
-    id="app"
-    class="bg-black flex justify-center"
-  >
-    <!-- Left glitches descending order -->
-    <img
-      src="/SideGlitchAlt3.png"
-      class="side-glitch-1 blink-image-1"
-      :class="{ showSideGlitch: !scrolling }"
+  <div class="h-full w-full">
+    <LoadingAnimation v-if="loading" />
+    <div
+      v-else
+      id="app"
+      class="bg-black flex justify-center"
     >
-    <img
-      src="/SideGlitchAlt1.png"
-      class="side-glitch-2 blink-image-3"
-      :class="{ showSideGlitch: !scrolling }"
-    >
-    <img
-      src="/SideGlitchAlt2.png"
-      class="side-glitch-3 blink-image-2"
-      :class="{ showSideGlitch: !scrolling }"
-    >
-    <img
-      src="/SideGlitchAlt4.png"
-      class="side-glitch-4 blink-image-3"
-      :class="{ showSideGlitch: !scrolling }"
-    >
-    <!-- Right glitches descending order -->
-    <img
-      src="/SideGlitchAlt5.png"
-      class="side-glitch-5 blink-image-2"
-      :class="{ showSideGlitch: !scrolling }"
-    >
-    <img
-      src="/SideGlitchAlt6.png"
-      class="side-glitch-6 blink-image-1"
-      :class="{ showSideGlitch: !scrolling }"
-    >
-    <img
-      src="/SideGlitchAlt7.png"
-      class="side-glitch-7 blink-image 3"
-      :class="{ showSideGlitch: !scrolling }"
-    >
-    <img
-      src="/SideGlitchAlt8.png"
-      class="side-glitch-8 blink-image-1"
-      :class="{ showSideGlitch: !scrolling }"
-    >
-
-    <CyCraftHomePage />
+      <!-- Left glitches descending order -->
+      <img
+        src="/SideGlitchAlt3.png"
+        class="side-glitch-1 blink-image-1"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <img
+        src="/SideGlitchAlt1.png"
+        class="side-glitch-2 blink-image-3"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <img
+        src="/SideGlitchAlt2.png"
+        class="side-glitch-3 blink-image-2"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <img
+        src="/SideGlitchAlt4.png"
+        class="side-glitch-4 blink-image-3"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <!-- Right glitches descending order -->
+      <img
+        src="/SideGlitchAlt5.png"
+        class="side-glitch-5 blink-image-2"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <img
+        src="/SideGlitchAlt6.png"
+        class="side-glitch-6 blink-image-1"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <img
+        src="/SideGlitchAlt7.png"
+        class="side-glitch-7 blink-image 3"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <img
+        src="/SideGlitchAlt8.png"
+        class="side-glitch-8 blink-image-1"
+        :class="{ showSideGlitch: !scrolling }"
+      >
+      <CyCraftHomePage />
+    </div>
   </div>
 </template>
 
@@ -80,15 +83,18 @@ var scrollStop = function (callback) {
   )
 }
 import CyCraftHomePage from './components/CyCraftHomePage.vue'
+import LoadingAnimation from './components/LoadingAnimation.vue'
 
 export default {
   name: 'App',
   components: {
     CyCraftHomePage,
+    LoadingAnimation,
   },
   data() {
     return {
       scrolling: false,
+      loading: true,
     }
   },
   computed: {},
@@ -109,6 +115,7 @@ export default {
 <style lang="sass">
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600;700;800&display=swap')
 @import './assets/styles.css'
+
 body
   overscroll-behavior-y: none
   overscroll-behavior-x: none
---
+++ src/components/LoadingAnimation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-full flex flex-col justify-center items-center bg-black">
+    <div
+      id="loading-animation"
+      class="w-auto bg-black"
+    ></div>
+  </div>
+</template>
+<script>
+import lottie from 'lottie-web'
+export default {
+  name: 'LoadingAnimation',
+  mounted() {
+    let loadingContainer = document.getElementById('loading-animation')
+    console.log(loadingContainer)
+    lottie.loadAnimation({
+      container: loadingContainer,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: `./animation/data.json`,
+    })
+  },
+}
+</script>
